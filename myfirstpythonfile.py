bdc7177393536ad0dadd50e8618f0c1107299182
Adam Adrian <adamadrians44@gmail.com>
2018-12-14 15:24:50 +0700

    added user login code and it is working.

--- myfirstpythonfile.py
+++ myfirstpythonfile.py
@@ -1,1 +1,9 @@
 some python code....
+
+
+coded me up some complicated stuff that helped a user log in to my app
+
+
+wow this is finally done and working
+now i can move on to showing the user their records, next.
+
